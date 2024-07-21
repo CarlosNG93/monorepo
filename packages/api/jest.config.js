cadaf42497130fbc7fa050dbdcacb3391f32b1da
Carlos Nuñez <carlosng1993@gmail.com>
2024-07-21 23:15:50 +0200

    chore: Update Jest configuration and test setup

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coverageDirectory: 'coverage',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], 
-  rootDir: '../',  
+  rootDir: 'src',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts']
 };
